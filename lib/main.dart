504c8a54783485fd185648e211bc73bdebb94d89
Sodento <ridvankilinc762@gmail.com>
2022-12-26 18:25:17 +0300

    test

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:ecomm_app/consts/consts.dart';
+import 'package:ecomm_app/views/splash_screen/splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // we are using getX so we have to change this material app into getmaterialapp
+    return GetMaterialApp(
+      title: appname,
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.transparent,
+        appBarTheme: AppBarTheme(backgroundColor: Colors.transparent),
+        fontFamily: regular,
+      ),
+      home: const SplashScreen(),
+    );
+  }
+}
